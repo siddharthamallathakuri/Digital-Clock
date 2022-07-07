5fd526ca4b7e450e6c9b0f46b80c0b5694082b9b
Siddhartha Malla <78959003+womunto@users.noreply.github.com>
2022-07-07 12:52:35 +0545

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,25 @@
+from cProfile import label
+from cgitb import text
+from tkinter import Label, Tk
+import time
+
+app_window = Tk()
+app_window.title("Digital Clock")
+app_window.geometry("420x150")
+app_window.resizable(1,1)
+
+text_front = ("Boulder", 68, "bold")
+background = "red"
+foreground = "white"
+border_width = 25
+
+label = Label(app_window, font=text_front, bg= background, fg=foreground, bd=border_width)
+label.grid(row=0, column=200)
+
+def digital_clock():
+    time_live = time.strftime("%H:%M:%S")
+    label.config(text=time_live)
+    label.after(200, digital_clock)
+
+digital_clock()
+app_window.mainloop()
